1c1be339a96884ceb7bdc5b194ed88c7429362a2
caoyabei <1039769138@qq.com>
2018-05-12 17:10:10 +0800

    ziixinhdj

---
+++ api/singer.js
@@ -0,0 +1,35 @@
+import jsonp from 'common/js/jsonp';
+import {option, commonParams} from 'api/config';
+
+export function getSingerList() {
+  let url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg';
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    g_tk: 1664029744
+  });
+  return jsonp(url, data, option);
+};
+
+export function getSingerDetail(id) {
+  let url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg';
+  const data = Object.assign({}, commonParams, {
+    g_tk: 5381,
+    loginUin: 0,
+    hostUin: 0,
+    platform: 'yqq',
+    needNewCode: 0,
+    order: 'listen',
+    begin: 0,
+    num: 100,
+    songstatus: 1,
+    singermid: id
+  });
+  return jsonp(url, data, option);
+}
