e4b35ab5e284fe06fe208366bc043fd58149df59
caoyabei <1039769138@qq.com>
2019-05-18 17:06:40 +0800

    zuijin

--- love/js/yiqi.js
+++ love/js/yiqi.js
@@ -11,16 +11,18 @@ window.onload=function(){
             alert("你是我见过的最漂亮善良可爱的女孩");
 
         }else if(Dianji==3){
-            alert("一生一世爱你");
+            alert("一生一世爱你～");
 
         }else if(Dianji==4){
-            alert("家务我全干");
+            alert("家务我全干～");
 
         }else if(Dianji==5){
-            alert("不藏私房钱");
+            alert("不藏私房钱～");
         }else if(Dianji==6){
-            alert("房子写你名");
+            alert("房子写你名～");
         }else if(Dianji==7){
+            alert("保大～");
+        }else if(Dianji==8){
             alert("工资全上交");
             Dianji=1;
         }
